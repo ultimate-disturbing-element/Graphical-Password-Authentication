bd092a8ea7992d1c3fe8cb30f74ae5fa34191a1d
Abhinav <A15930@aakashleap.com>
2023-12-20 21:53:55 +0530

    UI

--- client/src/components/AuthForm/AuthForm.js
+++ client/src/components/AuthForm/AuthForm.js
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
-import './AuthForm.css'; // Create a corresponding CSS file for styling
+import React, { useRef } from 'react';
+import './AuthForm.css';
 
 const AuthForm = () => {
-  const [isSignIn, setIsSignIn] = useState(true);
+  const containerRef = useRef(null);
 
-  const handleToggleForm = () => {
-    setIsSignIn((prev) => !prev);
-  };
+  const handleSignUpClick = () => containerRef.current.classList.add('right-panel-active');
+  const handleSignInClick = () => containerRef.current.classList.remove('right-panel-active');
 
   return (
     <body>
@@ -18,42 +17,45 @@ const AuthForm = () => {
 
       {/* Log In Form Section */}
       <section>
-        <div className="container" id="container">
-          <div className={`form-container ${isSignIn ? 'sign-in-container' : 'sign-up-container'}`}>
+        <div className="container" id="container" ref={containerRef}>
+          <div className="form-container sign-up-container">
             <form action="#">
-              <h1>{isSignIn ? 'Sign in' : 'Sign Up'}</h1>
+              <h1>Sign Up</h1>
               <div className="social-container">
                 <a href="" target="_blank" className="social"><i className="fab fa-github"></i></a>
                 <a href="" target="_blank" className="social"><i className="fab fa-codepen"></i></a>
                 <a href="" target="_blank" className="social"><i className="fab fa-google"></i></a>
               </div>
-              {isSignIn ? (
-                <>
-                  <span> Or sign in using E-Mail Address</span>
-                  <label>
-                    <input type="email" placeholder="Email" />
-                  </label>
-                  <label>
-                    <input type="password" placeholder="Password" />
-                  </label>
-                  <a href="#">Forgot your password?</a>
-                  <button>Sign In</button>
-                </>
-              ) : (
-                <>
-                  <span>Or use your Email for registration</span>
-                  <label>
-                    <input type="text" placeholder="Name" />
-                  </label>
-                  <label>
-                    <input type="email" placeholder="Email" />
-                  </label>
-                  <label>
-                    <input type="password" placeholder="Password" />
-                  </label>
-                  <button style={{ marginTop: '9px' }}>Sign Up</button>
-                </>
-              )}
+              <span>Or use your Email for registration</span>
+              <label>
+                <input type="text" placeholder="Name" />
+              </label>
+              <label>
+                <input type="email" placeholder="Email" />
+              </label>
+              <label>
+                <input type="password" placeholder="Password" />
+              </label>
+              <button style={{ marginTop: '9px' }}>Sign Up</button>
+            </form>
+          </div>
+          <div className="form-container sign-in-container">
+            <form action="#">
+              <h1>Sign in</h1>
+              <div className="social-container">
+                <a href="https://Github.com/YasinDehfuli" target="_blank" className="social"><i className="fab fa-github"></i></a>
+                <a href="https://Codepen.io/YasinDehfuli" target="_blank" className="social"><i className="fab fa-codepen"></i></a>
+                <a href="mailto:Ydehfuli@gmail.com" target="_blank" className="social"><i className="fab fa-google"></i></a>
+              </div>
+              <span>Or sign in using E-Mail Address</span>
+              <label>
+                <input type="email" placeholder="Email" />
+              </label>
+              <label>
+                <input type="password" placeholder="Password" />
+              </label>
+              <a href="#">Forgot your password?</a>
+              <button>Sign In</button>
             </form>
           </div>
           <div className="overlay-container">
@@ -61,12 +63,12 @@ const AuthForm = () => {
               <div className="overlay-panel overlay-left">
                 <h1>Log in</h1>
                 <p>Sign in here if you already have an account </p>
-                <button className="ghost mt-5" onClick={handleToggleForm}>Sign In</button>
+                <button className="ghost mt-5" id="signIn" onClick={handleSignInClick}>Sign In</button>
               </div>
               <div className="overlay-panel overlay-right">
                 <h1>Create Account!</h1>
                 <p>Sign up if you still don't have an account ... </p>
-                <button className="ghost" onClick={handleToggleForm}>Sign Up</button>
+                <button className="ghost" id="signUp" onClick={handleSignUpClick}>Sign Up</button>
               </div>
             </div>
           </div>
